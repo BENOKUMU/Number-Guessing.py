efdd48dbc0a37a20b11a492726a81b8390d59996
Benja <benjaminokumu12@gmail.com>
2021-11-26 17:08:14 +0300

    updated

--- Guessing.py
+++ Guessing.py
@@ -1,3 +1,4 @@
+# import random package
 import random
 attempts_list = []
 def show_score():
@@ -6,17 +7,17 @@ def show_score():
     else:
         print("The current high score is {} attempts".format(min(attempts_list)))
 def start_game():
-    random_number = int(random.randint(1, 10))
-    print("Hello traveler! Welcome to the game of guesses!")
+    random_number = int(random.randint(1, 100))
+    print("Hello gamer! Welcome to the game of guesses!")
     player_name = input("What is your name? ")
     wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
-    # // Where the show_score function USED to be
+    # Where the show_score function USED to be
     attempts = 0
     show_score()
     while wanna_play.lower() == "yes":
         try:
-            guess = input("Pick a number between 1 and 10 ")
-            if int(guess) < 1 or int(guess) > 10:
+            guess = input("Pick a number between 1 and 100 ")
+            if int(guess) < 1 or int(guess) > 100:
                 raise ValueError("Please guess a number within the given range")
             if int(guess) == random_number:
                 print("Nice! You got it!")
